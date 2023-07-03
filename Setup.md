89869fa203b0b156d83388d6079851ff8893151d
Silas Stulz <silas.stulz@gmail.com>
2023-07-03 18:19:39 +0200

    init

---
+++ Setup.md
@@ -0,0 +1,26 @@
+# Setup a Signer Node
+
+As setting up a signer node might be a technical task, I tried to simplify it as much as possible. Still, if something is unclear or you need support, contact me anytime at [silas.stulz@bcts.ch](mailto:silas.stulz@bcts.ch?subject=SwissDLT Setup Support)
+
+There are currently two possible ways to run a SwissDLT node, in a container or a source installation. For either of them you need to setup a VM.
+
+## 1. Virtual Machine Setup
+
+## 2. Virtual Machine installation
+
+- Connect to your virtual machine you setup via SSH.
+
+
+- Update the system:
+
+  ```sudo apt-get update```
+
+- Install geth
+
+  ```sudo add-apt-repository -y ppa:ethereum/ethereum```
+
+  ```sudo apt-get install ethereum```
+
+- 
+
+## 3. Docker Setup
---
+++ signer_setup.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Ensure the script is run with superuser privileges
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Ensure password is passed as argument
+if [ -z "$1" ]
+  then echo "Missing password argument. Usage: ./signer_setup.sh <PASSWORD>"
+  exit
+fi
+
+PASSWORD=$1
+
+# Update and install ethereum
+sudo apt-get update
+sudo add-apt-repository -y ppa:ethereum/ethereum
+sudo apt-get install ethereum
+
+# Create new geth account
+geth account new --datadir data --password "$PASSWORD" > /root/.ethereum/account.txt
+
+# Save password to file
+echo "$PASSWORD" > password.txt
+
+# Initialize geth with genesis.json
+geth init --datadir data genesis.json
+
+ACCOUNT_ADDRESS=$(grep -o -E '0x[a-fA-F0-9]{40}' /root/.ethereum/account.txt)
+echo $ACCOUNT_ADDRESS
+
+# Define service configuration
+CONFIG="[Unit]
+Description=SignerNode
+
+[Service]
+WorkingDirectory=/home/ubuntu
+User=ubuntu
+ExecStart=geth --syncmode full --mine --miner.etherbase $ACCOUNT_ADDRESS --datadir data --bootnodes enode://8e516c7bf3e27606256d01119fa303c37cf632756d824ecf33ded99322e483022c12603a8e7dbac9785045ff9e2cee04941ee7c3a8eed56c91913234154f7743@16.62.223.91:30309 --networkid=94 --port 30308 --unlock $ACCOUNT_ADDRESS --password password.txt
+Restart=always
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target"
+
+# Save service configuration to file
+echo "$CONFIG" > /lib/systemd/system/geth.service
+
+# Enable and start service
+sudo systemctl enable geth
+sudo systemctl start geth
