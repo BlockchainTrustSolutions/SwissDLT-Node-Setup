bf56d66b6995b35196fb639c7d7023393ae69eab
Silas Stulz <silas.stulz@gmail.com>
2023-07-03 18:28:42 +0200

    fix

--- signer_setup.sh
+++ signer_setup.sh
@@ -31,13 +31,16 @@ geth init --datadir data genesis.json
 ACCOUNT_ADDRESS=$(grep -o -E '0x[a-fA-F0-9]{40}' account.txt)
 echo $ACCOUNT_ADDRESS
 
+WORKDIR=$(pwd)
+CURRENT_USER=$(whoami)
+
 # Define service configuration
 CONFIG="[Unit]
 Description=SignerNode
 
 [Service]
-WorkingDirectory=/home/ubuntu
-User=ubuntu
+WorkingDirectory=$WORKDIR
+User=$CURRENT_USER
 ExecStart=geth --syncmode full --mine --miner.etherbase $ACCOUNT_ADDRESS --datadir data --bootnodes enode://8e516c7bf3e27606256d01119fa303c37cf632756d824ecf33ded99322e483022c12603a8e7dbac9785045ff9e2cee04941ee7c3a8eed56c91913234154f7743@16.62.223.91:30309 --networkid=94 --port 30308 --unlock $ACCOUNT_ADDRESS --password password.txt
 Restart=always
 RestartSec=5s
